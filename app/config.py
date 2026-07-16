5b652d378888c81f15be7d0b387f0be995e03179
KartikSharma0609 <kartiksharma06969@gmail.com>
2026-07-17 03:21:54 +0530

    Added Databases

--- app/config.py
+++ app/config.py
@@ -2,6 +2,13 @@ import os
 
 
 class Config:
+
     HOST = os.getenv("HOST", "0.0.0.0")
     PORT = int(os.getenv("PORT", 5000))
     DEBUG = os.getenv("DEBUG", "True").lower() == "true"
+
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
