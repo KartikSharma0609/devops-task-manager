9c2676767e2e3b49c7999f49c7a4b87bd3d7a6c0
KartikSharma0609 <kartiksharma06969@gmail.com>
2026-07-23 14:18:06 +0530

    testing git action

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,5 +1,9 @@
 def test_home(client):
 
+    print("\n--- ROUTES IN CI ---")
+    print(client.application.url_map)
+    print("--------------------\n")
+
     response = client.get("/system", follow_redirects=True)
 
     assert response.status_code == 200
