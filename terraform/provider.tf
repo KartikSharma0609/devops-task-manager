f4675a7c285800e1a860f6a1b441ae2a2aea0768
KartikSharma0609 <kartiksharma06969@gmail.com>
2026-09-07 18:42:36 +0530

    Enable Terraform S3 state locking

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,7 +7,14 @@ terraform {
       version = "~> 6.0"
     }
   }
-}
+
+  backend "s3" {
+      bucket = "kartik-devops-task-manager-terraform-state-689505268101"
+      key    = "devops-task-manager/terraform.tfstate"
+      region = "ap-south-1"
+      use_lockfile = true
+    }
+  }
 
 provider "aws" {
   region = var.aws_region
